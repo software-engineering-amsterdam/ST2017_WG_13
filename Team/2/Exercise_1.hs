aaeaf528ea76f32d2f6bb14bed0d71761c9a0558
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2017-09-11 15:06:33 +0200

    create team exercises
    with questions, to be deleted later

---
+++ Team/2/Exercise_1.hs
@@ -0,0 +1,19 @@
+-- Your programmer Red Curry has written the following function for generating lists of floating point numbers.
+
+-- > probs :: Int -> IO [Float]
+-- > probs 0 = return []
+-- > probs n = do
+-- >              p <- getStdRandom random
+-- >              ps <- probs (n-1) 
+-- >              return (p:ps)
+-- He claims that these numbers are random in the open interval (0..1)(0..1). Your task is to test whether 
+-- this claim is correct, by counting the numbers in the quartiles
+
+-- (0..0.25),[0.25..0.5),[0.5..0.75),[0.75..1)(0..0.25),[0.25..0.5),[0.5..0.75),[0.75..1)
+
+-- and checking whether the proportions between these are as expected.
+
+-- E.g., if you generate 10000 numbers, then roughly 2500 of them should be in each quartile.
+
+-- Implement this test, and report on the test results.
+
---
+++ Team/2/Exercise_7.hs
@@ -0,0 +1,19 @@
+-- Implementing and testing IBAN validation
+
+-- The International Bank Account Number (IBAN) was designed to facility international money transfer, 
+-- to uniquely identify bank accounts worldwide. It is described here, including a procedure for validating 
+-- IBAN codes. Write a function
+
+-- iban :: String -> Bool
+-- that implements this validation procedure.
+
+-- Next, test your implementation using some suitable list of examples.
+
+-- Note It is not enough to test only with correct examples. You should invent a way to test with incorrect 
+-- examples also.
+
+-- Can you automate the test process?
+
+-- Deliverables: Haskell program, concise test report, indication of time spent.
+
+
