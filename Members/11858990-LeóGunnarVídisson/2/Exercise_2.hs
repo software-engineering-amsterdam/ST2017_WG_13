757e791ebd4d3d87c9c6a029d759f00ccd439d88
Leó Gunnar vidisson <leo@tolvugerdi.is>
2017-09-12 15:33:18 +0200

    Klúður

--- Members/11858990-LeóGunnarVídisson/2/Exercise_2.hs
+++ Members/11858990-LeóGunnarVídisson/2/Exercise_2.hs
@@ -1,22 +1,39 @@
--- Recognizing triangles
+-- Implementing and testing IBAN validation
 
--- Write a program (in Haskell) that takes a triple of integer values as arguments and gives as output one of 
--- the following statements:
--- * Not a triangle (Geen driehoek) if the three numbers cannot occur as the lengths of the sides of triangle,
--- * Equilateral (Gelijkzijdig) if the three numbers are the lengths of the sides of an equilateral triangle,
--- * Rectangular (Rechthoekig) if the three numbers are the lengths of the sides of a rectangular triangle,
--- * Isosceles (Gelijkbenig) if the three numbers are the lengths of the sides of an isosceles 
---   (but not equilateral) triangle,
--- * Other (Anders) if the three numbers are the lengths of the sides of a triangle that is not equilateral, 
---   not rectangular, and not isosceles.
+-- The International Bank Account Number (IBAN) was designed to facility international money transfer, 
+-- to uniquely identify bank accounts worldwide. It is described here, including a procedure for validating 
+-- IBAN codes. Write a function
 
--- Here is a useful datatype definition:
+-- iban :: String -> Bool
+-- that implements this validation procedure.
 
--- > data Shape = NoTriangle | Equilateral 
--- >            | Isosceles  | Rectangular | Other deriving (Eq,Show)
--- Now define a function triangle :: Integer -> Integer -> Integer -> Shape with the right properties.
+-- Next, test your implementation using some suitable list of examples.
 
--- You may wish to consult wikipedia. Indicate how you tested or checked the correctness of the program.
+-- Note It is not enough to test only with correct examples. You should invent a way to test with incorrect 
+-- examples also.
+
+-- Can you automate the test process?
 
 -- Deliverables: Haskell program, concise test report, indication of time spent.
 
+module Iban where
+
+
+--Tuples
+countryAndLength = [("AL", 28), ("AD", 24), ("AT", 20), ("AZ", 28), ("BH", 22),("BE", 16), ("BA", 20), ("BG", 22), ("HR", 21), ("CY", 28), ("CZ", 24), ("DK", 18), ("EE", 20), ("FO", 18), ("FI", 18), ("FR", 27), ("GE", 22), ("DE", 22), ("GI", 23), ("GR", 27), ("GL", 18), ("HU", 28), ("IS", 26), ("IE", 22), ("IL", 23), ("IT", 27), ("JO", 30), ("KW", 30), ("LV", 21), ("LB", 28), ("LI", 21), ("LT", 20), ("LU", 20), ("MK", 19), ("MT", 31), ("MU", 30), ("MD", 24), ("MC", 27), ("ME", 22), ("NL", 18), ("NO", 15), ("PK", 24), ("PL", 28), ("PT", 25), ("QA", 29), ("RO", 24), ("SM", 27), ("SA", 24), ("RS", 22), ("SK", 24), ("SI", 19), ("ES", 24), ("SE", 24), ("CH", 21), ("TN", 24), ("TR", 26), ("AE", 23), ("GB", 22)] 
+
+
+
+input :: String -> String
+input countryCode = take(2) countryCode
+
+
+validate ::String -> Bool
+validate a = ( input a, length (filter (/=' ') a) ) `elem` countryAndLength 
+
+
+
+{-
+iban :: String -> Bool
+iban 
+-}
