b3ff37b1ffde375cd0ac9287e4647e37690703c3
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2017-09-12 11:01:04 +0200

    first attempt at Q1 and 5

--- Members/11721677-PaulSpencer/2/Exercise_1.hs
+++ Members/11721677-PaulSpencer/2/Exercise_1.hs
@@ -2,8 +2,9 @@ module Lab2 where
 import Data.List
 import Data.Char
 import System.Random
+import Control.Monad
 import Test.QuickCheck
-
+import Test.QuickCheck.Monadic
 -- Your programmer Red Curry has written the following function for generating lists of floating point numbers.
 
 -- > probs :: Int -> IO [Float]
@@ -26,6 +27,60 @@ import Test.QuickCheck
 probs :: Int -> IO [Float]
 probs 0 = return []
 probs n = do
-             p <- getStdRandom random
-             ps <- probs (n-1) 
-             return (p:ps)
+    p <- getStdRandom random
+    ps <- probs (n-1) 
+    return (p:ps)
+
+closedIntervalProperty:: Positive Int -> Property
+closedIntervalProperty (Positive n) = monadicIO $ do
+    testprobs <- run (probs n)
+    assert $ (not $ elem 0 testprobs) && (not $ elem 1 testprobs) 
+
+quartile::Float -> Int
+quartile x 
+    | x < 0.25  = 0
+    | x < 0.5   = 1
+    | x < 0.75  = 2
+    | otherwise = 3
+    
+runXTimes::Int -> Int -> IO [Float]
+runXTimes _ 0 = return []
+runXTimes n c = liftM2 (++) (probs n) (runXTimes n (c-1))
+
+runLotsOfTimes::Int -> IO [Int]
+runLotsOfTimes n = liftM (map quartile) (runXTimes n 1000) 
+
+
+average::[Int] -> Float
+average xs = (fromIntegral (sum xs)) / (fromIntegral (length xs))
+
+equalsDelta :: Float -> Float -> Float -> Bool
+equalsDelta n target delta 
+    | n > (target + delta) = False
+    | n < (target - delta) = False
+    | otherwise = True
+
+statisticallySignificantProperty:: Positive Int -> Property
+statisticallySignificantProperty (Positive n) = monadicIO $ do
+    randomQuartiles <- run (runLotsOfTimes n)
+    assert $ equalsDelta (average randomQuartiles) 1.5 0.1
+
+
+
+probs' :: Int -> IO [Float]
+probs' 0 = return []
+probs' n = do
+    p <- getStdRandom $ randomR (0,0.25)
+    q <- getStdRandom $ randomR (0.25,0.5)
+    r <- getStdRandom $ randomR (0.5,0.75)
+    s <- getStdRandom $ randomR (0.75,1)
+    ps <- probs' (n-1)
+    return (p:q:r:s:ps)
+
+runXTimes'::Int -> Int -> IO [Float]
+runXTimes' _ 0 = return []
+runXTimes' n c = liftM2 (++) (probs n) (runXTimes' n (c-1))
+
+runLotsOfTimes::Int -> IO [Int]
+runLotsOfTimes n = liftM (map quartile) (runXTimes n 1000) 
+    
--- Members/11721677-PaulSpencer/2/Exercise_5.hs
+++ Members/11721677-PaulSpencer/2/Exercise_5.hs
@@ -1,3 +1,12 @@
+module Lab2 where
+import Data.List
+import Data.Char
+import System.Random
+import Control.Monad
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import qualified Data.Set as S
+
 -- Recognizing and generating derangements
 
 -- A derangement of the list [0..n-1] of natural numbers is a permutation π of the list with the property 
@@ -21,3 +30,36 @@
 -- Can you automate the test process?
 
 -- Deliverables: Haskell program, concise test report, indication of time spent.
+
+deran::Int -> [[Int]]
+deran n = filter areSwapped $ permutations [0.. (n-1)]
+deran' n = [0.. (n-1)]
+
+areSwapped::[Int] -> Bool
+areSwapped perm = all (==False) $ zipWith (==) perm [0..]
+
+isDerangement::[Int] -> [[Int]] -> Bool
+isDerangement [] [[]] = True
+isDerangement [_] [] = True
+isDerangement os@(x:xs) dss@(ys:yss)
+    | (derangementSize (length os)) /= (length dss)           = False
+    | not $ all (==(length os)) (map length dss)              = False
+    | any (==True) $ concat $ map (zipWith (==) os) dss       = False
+    | not $ all (==True) $ map (sameElems os) dss             = False
+    | otherwise  = True
+
+
+-- inspired by Ben Millwood's answer https://stackoverflow.com/questions/15319136/how-to-compare-two-lists-in-haskell
+sameElems xs ys = S.fromList xs == S.fromList ys
+
+
+-- formula for derangement size taken from http://oeis.org/wiki/Number_of_derangements
+derangementSize 1 = 0 
+derangementSize n = ((derangementSize (n-1)) * n) + ((-1)^n)
+
+tmp os@(x:xs) dss@(ys:yss) = concat $ map (zipWith (==) os) dss
+
+    -- | any $ concat $ map (zipWith (==) os) dss      = False
+    -- | all $ concat $ zipWith (elem os)
+
+-- implement https://link.springer.com/content/pdf/10.1007/BF01933579.pdf to prove
