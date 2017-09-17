58b3018e26d2a3165b8992dc4e7129132a69bb06
Toine Khonraad <a.khonraad@khonraad.nl>
2017-09-17 10:38:12 +0200

    Removed Paul's comments

--- Members/11424907-ToineKhonraad/2/Lab2_Exercise4.hs
+++ Members/11424907-ToineKhonraad/2/Lab2_Exercise4.hs
@@ -13,27 +13,6 @@ left -<- right = all (True ==) [ x `elem` right  | x <- left  ]
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation left right = left -<- right && right -<- left
 
---proposed properties weakest to strongest
---   
--- same length
--- all values in left are in right plus above
--- all values in right are in left plus above
--- the values in left and right are in same proportion plus above
--- (if I do not say plus above, then I do not see how one some of these can be subsets, they would be 
--- incompatable i.e.:
---     same length ['a','a','b'] -> ['a','b','a'] ['b','a','a'] ['b','b','a']['a','a','a'] ['b','b','b']['x','y','z'] 
---     all values in left in right ->  ['a','b','a'] ['b','a','a'] ['b','b','a']['a','a','a'] ['a','a','a','b'] 
---     all values in right are left -> ['a','b','a'] ['b','a','a'] ['b','b','a'] ['b','b','b']  ['a','a','a','b'] 
---     values are in same proportion -> ['a','b','a'] ['b','a','a'] ['b','b','a','a','a','a']
--- vs     
---     same length ['a','a','b'] -> ['a','b','a'] ['b','a','a'] ['b','b','a']['a','a','a'] ['b','b','b']['x','y','z'] 
---     all values in left in right ->  ['a','b','a'] ['b','a','a'] ['b','b','a']['a','a','a']  
---     all values in right are left -> ['a','b','a'] ['b','a','a'] ['b','b','a'] 
---     values are in same proportion -> ['a','b','a'] ['b','a','a'] 
--- 
-
-
-
 propSelfPermutation :: Eq a => [a] -> Bool
 propSelfPermutation a =  isPermutation a a
 
