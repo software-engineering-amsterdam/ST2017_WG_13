ebd6fce75afc7d26441908001b0b7a4839711823
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2017-09-17 15:15:33 +0200

    update Exercise 5 to test correct thing

--- Members/11721677-PaulSpencer/2/Exercise_6.hs
+++ Members/11721677-PaulSpencer/2/Exercise_6.hs
@@ -2,15 +2,6 @@ module Lab2 where
 import Data.List
 import Data.Char
 import Test.QuickCheck
-import Data.Ascii
--- Implementing and testing ROT13 encoding
-
--- First, give a specification of ROT13.
-
--- Next, give a simple implementation of ROT13.
-
--- Finally, turn the specification into a series of QuickCheck testable properties, and use these to test 
--- your implementation.
 
 -- Specification from Wikipedia (https://en.wikipedia.org/wiki/ROT13):
 -- ROT13 .. is a simple letter substitution cipher that replaces a letter with the letter 13 letters 
@@ -21,13 +12,12 @@ rot13::String -> String
 --rot13 xs = xs, pass first test
 --rot13 xs = map (chr . (1+) . ord) xs
 --rot13 xs = map (\x -> if isLetter x then (chr . (1+) . ord) x else x) xs
-rot13 xs = map (\x -> if isBasicLatin x then encode x else x) xs
+rot13 xs = map (\x -> if elem x (uc ++ lc) then encode x else x) xs
     where 
-        isBasicLatin x = (isAsciiLower x) || (isAsciiUpper x)
-        rotate n xs = (drop n xs) ++ (take n xs)
         uc = ['A'..'Z']
         lc = ['a'..'z']
-        letterSwaps = zip (uc ++ lc) ((rotate 1 uc) ++ (rotate 1 lc))
+        rotate n xs = (drop n xs) ++ (take n xs)
+        letterSwaps = zip (uc ++ lc) ((rotate 13 uc) ++ (rotate 13 lc))
         encode l = snd $ head $ dropWhile ((l/=) . fst) letterSwaps
 
 -- length remains the same 
@@ -35,8 +25,7 @@ rot13 xs = map (\x -> if isBasicLatin x then encode x else x) xs
 -- Uppercase remain upper case
 -- Lowercase remains lower case
 -- If no letters there will be no change
--- If mixture of letters and non letters then pattern of changed and unchanged should match pattern of letter 
---   and non-letter
+-- If mixture of letters and non letters change
 -- If run on itself it should return original string
 
 prop_sameLength xs = length xs == (length $ rot13 xs)
@@ -49,3 +38,23 @@ prop_allNonLettersStay xs = (all (not . isLetter) xs) ==> all (==True) $ zipWith
 
 prop_UppersRemainUpper xs = (all (isAsciiUpper) xs) ==> all isAsciiUpper $ rot13 xs 
 
+prop_LowersRemainLower xs = (all (isAsciiLower) xs) ==> all isAsciiLower $ rot13 xs 
+
+-- have to take into account that unicode alphas do not change
+nonLatinAlpha x = (not $ isAsciiUpper x) && (not $ isAsciiLower x)
+prop_NoAlphaRemainsSame xs = (all nonLatinAlpha xs) ==> xs == (rot13 xs)
+
+aAndNonA xs = (elem True nonalphas) && (elem False nonalphas) 
+    where 
+        nonalphas = map nonLatinAlpha xs
+
+prop_MixAlphaNonAlphaChanges xs = (aAndNonA xs) ==> xs /= (rot13 xs)
+
+prop_MixNonAlphasAreSame xs = (aAndNonA xs) ==> (filter nonLatinAlpha xs) == (filter nonLatinAlpha (rot13 xs))
+
+prop_MixAlphasAreAllDifferent xs =  (aAndNonA xs) ==> all (==False) $ zipWith (==) origA rot13A
+    where
+        origA = (filter (not . nonLatinAlpha) xs)
+        rot13A = (filter (not . nonLatinAlpha) (rot13 xs))
+
+prop_IsReflexive xs = xs == (rot13 $ rot13 xs)
