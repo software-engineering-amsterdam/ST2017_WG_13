6004dafeb9573341280223c7381877328e9a1933
olimoli9160 <michael_olivari@hotmail.com>
2017-09-17 16:53:17 +0200

    repushed with correct names

--- Team/2/Exercise_2.hs
+++ Team/2/Exercise_2.hs
@@ -1,27 +1,133 @@
-module Lab2 where
+module Lab1 where
 import Data.List
-import Data.Char
-import System.Random
 import Test.QuickCheck
--- Recognizing triangles
 
--- Write a program (in Haskell) that takes a triple of integer values as arguments and gives as output one of 
--- the following statements:
--- * Not a triangle (Geen driehoek) if the three numbers cannot occur as the lengths of the sides of triangle,
--- * Equilateral (Gelijkzijdig) if the three numbers are the lengths of the sides of an equilateral triangle,
--- * Rectangular (Rechthoekig) if the three numbers are the lengths of the sides of a rectangular triangle,
--- * Isosceles (Gelijkbenig) if the three numbers are the lengths of the sides of an isosceles 
---   (but not equilateral) triangle,
--- * Other (Anders) if the three numbers are the lengths of the sides of a triangle that is not equilateral, 
---   not rectangular, and not isosceles.
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
 
--- Here is a useful datatype definition:
+forall :: [a] -> (a -> Bool) -> Bool
+forall = flip all
 
--- > data Shape = NoTriangle | Equilateral 
--- >            | Isosceles  | Rectangular | Other deriving (Eq,Show)
--- Now define a function triangle :: Integer -> Integer -> Integer -> Shape with the right properties.
+-- Q2
 
--- You may wish to consult wikipedia. Indicate how you tested or checked the correctness of the program.
+-- Program 
+data Shape = NoTriangle | Equilateral | Isosceles | Rectangular | Other deriving (Eq, Show)
 
--- Deliverables: Haskell program, concise test report, indication of time spent.
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle a b c = triangleType (sort [a,b,c])
 
+triangleType :: [Integer] -> Shape
+triangleType [a,b,c]
+   | not (isTriangle a b c) = NoTriangle
+   | isEquilateral a b c = Equilateral
+   | isRectangular a b c = Rectangular
+   | isIsosceles a b c = Isosceles
+   | otherwise = Other
+
+{- Triangle Classification Properties -}
+
+-- A triangle is defined by the sum of the smaller sides being greater than the largest side.
+isTriangle :: Integer -> Integer -> Integer -> Bool
+isTriangle a b c = ((a /= 0) && (b /= 0) && (c /= 0)) && ((a + b) > c)
+
+-- All sides of triangle are same length
+isEquilateral :: Integer -> Integer -> Integer -> Bool
+isEquilateral a b c = a == b && b == c
+
+-- Two sides are the same length
+isIsosceles :: Integer -> Integer -> Integer -> Bool
+isIsosceles a b c = (a == b) || (b == c) 
+
+-- Pythagorean Theorem
+isRectangular :: Integer -> Integer -> Integer -> Bool
+isRectangular a b c = (a^2 + b^2) == c^2
+
+{-
+Test Procedure: 
+
+Testing the correctness of the triangle program is done by testing the order in which inputs
+are received. This correctness lies in the property that triangles with the same side lengths 
+belong to the same triangle classification, no matter the ordering of the sides (i.e. the triangles
+rotation along its centerpoint).
+
+To perform this test, the derangement set of the input is used to compare their resulting shape
+with that of the original input.
+
+A passing test shows a differing order of inputs produces equivalent shaped triangles.
+A failed test shows a differing order of inputs produces inequivalent shaped triangles.
+-}
+
+triangleTest :: Integer -> Integer -> Integer -> Bool
+triangleTest a b c = (triangle a b c == triangle b c a) && (triangle a b c == triangle c a b)
+
+{-
+Test report:
+
+quickCheck on the triangleTest property passes all tests.
+
+First 10 checks are shown below with the corresponding input:
+
+1. 
+Passed:
+0
+0
+0
+
+2. 
+Passed:
+1
+-1
+-1
+
+3. 
+Passed:
+0
+0
+0
+
+4. 
+Passed:
+1
+-1
+-2
+
+5. 
+Passed:
+1
+2
+-4
+
+6. 
+Passed:
+0
+4
+4
+
+7. 
+Passed:
+-3
+-6
+3
+
+8. 
+Passed:
+-7
+-5
+-1
+
+9. 
+Passed:
+4
+0
+-7
+
+10. 
+Passed:
+-7
+-4
+0
+
+
+Total Time Spent: ~1.5 hrs
+-}
