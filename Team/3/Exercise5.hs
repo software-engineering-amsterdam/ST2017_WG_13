0ac488458196cc77e2cc730c74cef526702dd01a
Toine Khonraad <a.khonraad@khonraad.nl>
2017-09-22 20:28:19 +0200

    First draft exercise 5, tests waiting for completion on ex 3,4

---
+++ Team/3/Exercise5.hs
@@ -0,0 +1,34 @@
+module Lab3_Exercise5 where
+  
+import Lecture3
+
+normalForm :: Form -> Form
+normalForm = (nnf.arrowfree) 
+
+
+type Clause  = [ Int    ]
+type Clauses = [ Clause ]
+
+preConditionError = error "Precondition not met."
+
+{- 
+    Precondition: Prop Name >= 1 
+-}
+toProperty :: Form -> Int
+toProperty ( Prop x )         = if (x < 1) then preConditionError else x
+toProperty ( Neg ( Prop x ) ) = if (x < 1) then preConditionError else -x
+
+toClause :: Form -> Clause
+toClause ( Dsj list )         = map toProperty list
+toClause formTerm             = toClause $ Dsj [formTerm] 
+
+
+{- 
+    Precondition: Form in CNF (nnf and arrowfree and prop names >= 1)
+-}
+toClauses :: Form -> Clauses
+toClauses ( Equiv _ _       ) = preConditionError
+toClauses ( Impl _ _        ) = preConditionError
+toClauses ( Cnj clauseList  ) = map toClause clauseList
+toClauses formTerm            = toClauses $ Cnj [formTerm]
+
