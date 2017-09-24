f77568a14f98f8d2120e880ebac25e2f2c949227
Toine Khonraad <a.khonraad@khonraad.nl>
2017-09-24 21:44:23 +0200

    Push latest code as well.

--- Team/3/Exercise_5.hs
+++ Team/3/Exercise_5.hs
@@ -1,6 +1,12 @@
 module Exercise_5 where
   
+import Test.QuickCheck
+
 import Lecture3
+
+import Exercise_1
+import Exercise_3
+import Exercise_4
 
 normalForm :: Form -> Form
 normalForm = (nnf.arrowfree) 
@@ -17,22 +23,36 @@ preConditionError = error "Precondition not met."
 toProperty :: Form -> Int
 toProperty ( Prop x )         = if (x < 1) then preConditionError else x
 toProperty ( Neg ( Prop x ) ) = if (x < 1) then preConditionError else -x
+toProperty p@_                = error $ "syntax in form: " ++ show p
 
 toClause :: Form -> Clause
 toClause ( Dsj list )         = map toProperty list
 toClause formTerm             = toClause $ Dsj [formTerm] 
 
 
+{- 
+    Precondition: Form in CNF (nnf and arrowfree and prop names >= 1)
+-}
 toClauses :: Form -> Clauses
 toClauses ( Equiv _ _       ) = preConditionError
 toClauses ( Impl _ _        ) = preConditionError
 toClauses ( Cnj clauseList  ) = map toClause clauseList
 toClauses formTerm            = toClauses $ Cnj [formTerm]
 
-{- 
-    Precondition: Form in CNF (nnf and arrowfree and prop names >= 1)
--}
-cnf2cls :: Form -> Clauses
+-- define cnf2cls as that is the required name for the diliverable
 cnf2cls = toClauses
 
+-- To test this I went the other way around: clfToForm creates a form out
+-- of the CLS, to test the equivalence of that to the orginal Form.
+toProp n 
+    | n < 0     = Neg (Prop (abs n))
+    | otherwise = Prop n
 
+cls2form cs = Cnj (map (\fs -> Dsj ((map toProp) fs)) cs)
+
+prop_equivalent :: Form -> Bool
+prop_equivalent form = (formToCnf form)
+                       `equiv`
+                       (cls2form $ cnf2cls $ formToCnf form)
+                       
+test_equivalent = verboseCheck prop_equivalent
