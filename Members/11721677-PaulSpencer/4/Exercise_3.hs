098a658e7a1a7118c01edd86a6275337f7ccd2d3
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2017-10-01 17:03:07 +0200

    md Ex 2

--- Members/11721677-PaulSpencer/4/Exercise_3.hs
+++ Members/11721677-PaulSpencer/4/Exercise_3.hs
@@ -1,49 +1,106 @@
 module Exercise_3 where
-
-import Data.List
 import System.Random
-import Test.QuickCheck
 import SetOrd
+import Test.QuickCheck
+import Data.List
 import Exercise_2
-import Debug.Trace
+
 {-
     notes for this exercise can be found at:
-    https://github.com/software-engineering-amsterdam/ST2017_WG_13/tree/master/Team/4/Exercise_2.md
+    https://github.com/software-engineering-amsterdam/ST2017_WG_13/tree/master/Team/4/Exercise_3.md
 -}
 
-{-
-    Implement operations for set intersection, set union and set difference, for the datatype Set 
-    defined in SetOrd.hs. Next, use automated testing to check that your implementation is correct. 
-    First use your own generator, next use QuickCheck.
+-- impl
+intersect_SetInt :: (Ord a) => Set a -> Set a -> Set a
+intersect_SetInt (Set a) (Set b) = list2set (intersect a b)
 
-    (Deliverables: implementations, test properties, short test report, indication of time spent.)
--}
+union_SetInt :: (Ord a) => Set a -> Set a -> Set a
+union_SetInt (Set a) (Set b) = list2set (union a b)
 
--- implement intersect
-intersectSet :: (Ord a) => Set a -> Set a -> Set a 
-intersectSet _ (Set []) =  emptySet
-intersectSet (Set []) _ =  emptySet
-intersectSet s1@(Set (x:xs)) s2 
-    | s1 == s2 = s1
-    | inSet x s2 = insertSet x (intersectSet (Set xs) s2)
-    | otherwise = intersectSet (Set xs) s2
+difference_SetInt:: (Ord a) => Set a -> Set a -> Set a
+difference_SetInt (Set a) (Set b) = list2set ( a \\ b)
 
--- implement diff
+-- tests
+test_InBothSets          = setIntTester2 1 100 prop_InBothSets
+test_InBothSets'         = verboseCheck prop_InBothSets
+test_IntersectOrNotSet1  = setIntTester2 1 100 prop_IntersectOrNotSet1
+test_IntersectOrNotSet1' = verboseCheck prop_IntersectOrNotSet1
+test_IntersectOrNotSet2  = setIntTester2 1 100 prop_IntersectOrNotSet2
+test_IntersectOrNotSet2' = verboseCheck prop_IntersectOrNotSet2
 
-diffSet :: (Ord a) => Set a -> Set a -> Set a 
-diffSet s1 (Set []) =  s1
-diffSet (Set []) s2 =  s2
-diffSet s1@(Set (x:xs)) s2 
-    | s1 == s2 = emptySet
-    | inSet x s2 = diffSet (Set xs) (deleteSet x s2)
-    | otherwise = insertSet x (diffSet (Set xs) s2)
+test_inAtLeastOneSet = setIntTester2 1 100 prop_inAtLeastOneSet
+test_inAtLeastOneSet' = verboseCheck prop_inAtLeastOneSet
+test_AllSet1InUnion = setIntTester2 1 100  prop_AllSet1InUnion
+test_AllSet1InUnion' = verboseCheck  prop_AllSet1InUnion
+test_AllSet2InUnion = setIntTester2 1 100  prop_AllSet2InUnion
+test_AllSet2InUnion' = verboseCheck  prop_AllSet2InUnion
+test_matchImpl = setIntTester2 1 100 prop_matchedInternalImpl
+test_matchImpl' = verboseCheck prop_matchedInternalImpl
 
--- implement union (already done, so lets try a different way)
+test_NotInBothSets = setIntTester2 1 100 prop_NotInBothSets
+test_NotInBothSets' = verboseCheck prop_NotInBothSets
 
-unionSet2:: (Ord a) => Set a -> Set a -> Set a 
-unionSet2 (Set xs) (Set ys) = list2set (xs ++ ys)
+testAll = do
+  test_InBothSets
+  test_IntersectOrNotSet1
+  test_IntersectOrNotSet2
+  test_inAtLeastOneSet 
+  test_AllSet1InUnion
+  test_AllSet2InUnion
+  test_matchImpl
+  test_NotInBothSets
 
--- test intesect with random
+testAllQc = do
+  test_InBothSets'
+  test_IntersectOrNotSet1'
+  test_IntersectOrNotSet2'
+  test_inAtLeastOneSet'
+  test_AllSet1InUnion'
+  test_AllSet2InUnion'
+  test_matchImpl'
+  test_NotInBothSets'
+  
+-- properties
+prop_InBothSets:: Set Int -> Set Int -> Bool
+prop_InBothSets s1 s2 = all (==True) [(inSet x s1) && (inSet x s2) | x <- set2list s3  ]
+    where 
+        s3 = intersect_SetInt s1 s2
+
+prop_IntersectOrNotSet2:: Set Int -> Set Int -> Bool
+prop_IntersectOrNotSet2 s1 s2 = all (==True) [(inSet x s3) || (not $ inSet x s2) | x <- set2list s1  ]
+    where 
+        s3 = intersect_SetInt s1 s2
+
+prop_IntersectOrNotSet1:: Set Int -> Set Int -> Bool
+prop_IntersectOrNotSet1 s1 s2 = all (==True) [(inSet x s3) || (not $ inSet x s1) | x <- set2list s2  ]
+    where 
+        s3 = intersect_SetInt s1 s2
+
+prop_NotInBothSets:: Set Int -> Set Int -> Bool
+prop_NotInBothSets s1 s2 = all (==True) [not $ insetXor x s1 s2 | x <- set2list s3  ]
+    where 
+        s3 = difference_SetInt s1 s2
+        insetXor x s1 s2 = ((inSet x s1) || (inSet x s2)) && (not (inSet x s1) && (inSet x s2))
+        
+prop_inAtLeastOneSet:: Set Int -> Set Int -> Bool
+prop_inAtLeastOneSet s1 s2 = all (==True) [(inSet x s1) || (inSet x s2) | x <- set2list s3  ]
+    where 
+        s3 = union_SetInt s1 s2
+
+prop_AllSet1InUnion:: Set Int -> Set Int -> Bool
+prop_AllSet1InUnion s1 s2 = all (==True) [(inSet x s3) | x <- set2list s1  ]
+    where 
+        s3 = union_SetInt s1 s2
+
+prop_AllSet2InUnion:: Set Int -> Set Int -> Bool
+prop_AllSet2InUnion s1 s2 = all (==True) [(inSet x s3) | x <- set2list s2  ]
+    where 
+        s3 = union_SetInt s1 s2
+        
+prop_matchedInternalImpl::Set Int -> Set Int -> Bool
+prop_matchedInternalImpl s1 s2 = (unionSet s1 s2) == (union_SetInt s1 s2)
+
+-- from scratch tester
 setIntTester2 :: Int -> Int -> (Set Int -> Set Int -> Bool) -> IO ()
 setIntTester2 current total property = 
     if current == total then 
@@ -57,35 +114,3 @@ setIntTester2 current total property =
                 setIntTester2 (current+1) total property
         else 
             error ("failed test on: " ++ show si1++ " and  " ++ show si2)
-
-prop_InBothSets:: Set Int -> Set Int -> Bool
-prop_InBothSets s1 s2 = all (==True) [(inSet x s1) && (inSet x s2) | x <- set2list s3  ]
-    where 
-        s3 = intersectSet s1 s2
-
-prop_NotInBothSets:: Set Int -> Set Int -> Bool
-prop_NotInBothSets s1 s2 = all (==True) [(not $ inSet x s1) || (not $ inSet x s2) | x <- set2list s3  ]
-    where 
-        s3 = diffSet s1 s2
-
-prop_inAtLeastOneSet:: Set Int -> Set Int -> Bool
-prop_inAtLeastOneSet s1 s2 = all (==True) [(inSet x s1) || (inSet x s2) | x <- set2list s3  ]
-    where 
-        s3 = unionSet2 s1 s2
-
-prop_matchedInternalImpl::Set Int -> Set Int -> Bool
-prop_matchedInternalImpl s1 s2 = (unionSet s1 s2) == (unionSet2 s1 s2)
-
---- test with from scrath
-
-test_InBothSets = setIntTester2 1 100 prop_InBothSets
-test_NotInBothSets = setIntTester2 1 100 prop_NotInBothSets
-test_inAtLeastOneSet = setIntTester2 1 100 prop_inAtLeastOneSet
-test_matchImpl = setIntTester2 1 100 prop_matchedInternalImpl
-
--- test intersect with QuickCheck
-
-test_InBothSets' = verboseCheck prop_InBothSets
-test_NotInBothSets' = verboseCheck prop_NotInBothSets
-test_inAtLeastOneSet' = verboseCheck prop_inAtLeastOneSet
-test_matchImpl' = verboseCheck prop_matchedInternalImpl
