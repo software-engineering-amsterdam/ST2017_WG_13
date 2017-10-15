1589b2ee0c3b3738e6daceb5e50e5d535f1dfc0e
olimoli9160 <michael_olivari@hotmail.com>
2017-10-15 15:31:40 +0200

    Small update to 4

--- Team/6/Exercise_4.hs
+++ Team/6/Exercise_4.hs
@@ -9,32 +9,72 @@ import Exercise_3
 
 -- time taken: ~45 mins
 
-falsePrimes :: Int -> [Integer] -> IO Integer
+falsePrimes :: Int -> [Integer] -> IO [Integer]
+falsePrimes _ [] = return []
 falsePrimes k (x:xs) = 
             do 
                 testFault <- primeTestsF k x
                 if testFault then
-                    return x
+                    return (x:[])
                 else
                     falsePrimes k xs
 
-test :: Int -> Int -> IO Integer
+-- Test function for easy input
+test :: Int -> Int -> IO [Integer]
 test k n = falsePrimes k (take n composites')
+
+------------------ Testing Results --------------------------
 
 -- Composite '9' fails with k = 1 and 1000 composites
 {-
 *Exercise_4> test 1 1000
-45
+[45]
 *Exercise_4> test 1 1000
-9
+[9]
 *Exercise_4> test 1 1000
-25
+[25]
 *Exercise_4> test 1 1000
-15
+[15]
 *Exercise_4> test 1 1000
-237
+[237]
 *Exercise_4> test 1 1000
-27
+[27]
+-}
+
+-- composite '25' fails with k = 2
+{-
+*Exercise_4> test 2 1000
+[403]
+*Exercise_4> test 2 1000
+[451]
+*Exercise_4> test 2 1000
+[805]
+*Exercise_4> test 2 1000
+[133]
+*Exercise_4> test 2 1000
+[427]
+*Exercise_4> test 2 1000
+[25]
+*Exercise_4> test 2 1000
+[85]
+-}
+
+-- composite '85' fails with k = 3
+{-
+*Exercise_4> test 3 1000
+[1105]
+*Exercise_4> test 3 1000
+[1105]
+*Exercise_4> test 3 1000
+[341]
+*Exercise_4> test 3 1000
+[]
+*Exercise_4> test 3 1000
+[85]
+*Exercise_4> test 3 1000
+[1105]
+*Exercise_4> test 3 1000
+[1105]
 -}
 
 -- increasing K increases the accuracy of the primeTestsF, reducing the amount of 
